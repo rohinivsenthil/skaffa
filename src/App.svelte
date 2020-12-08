0fc1d08734d3c19660828d66671f561cb34a7984
Rohini Senthil <rohinivsenthil@gmail.com>
2020-12-08 13:04:58 +0530

    (feat) add button component

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,27 @@
 </script>
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Helvetica:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Arial:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Button />
+	<Button>button</Button>
+	<Button variant="outlined">button</Button>
+	<Button variant="text">button</Button>
+	<Button disabled>button</Button>
+	<Button variant="outlined" disabled>button</Button>
+	<Button variant="text" disabled>button</Button>
 </main>
 
 <style>
+	:root {
+		--primary: 98, 0, 238;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ src/components/Button/index.svelte
@@ -0,0 +1,91 @@
+<script>
+  export let variant = 'contained';
+  export let onClick;
+  export let disabled = false;
+  /**
+   * TODO: add in following props
+   */
+  // export let disableElevation = 'false';
+  // export let fullWidth = 'false';
+  // export let size = 'medium';
+  // export let color = 'primary'
+
+  const btnClass = 'spunk-btn ' + `spunk-btn-${variant}`;
+</script>
+
+<button 
+  class={btnClass}
+  on:click={onClick}
+  disabled={disabled}
+  type="button"
+>
+  <slot></slot>
+</button>
+
+<style>
+  .spunk-btn {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    overflow: visible;
+    vertical-align: middle;
+
+    min-width: 64px;
+    padding: 6px 16px 6px 16px;
+    border: none;
+    border-radius: 4px;
+    margin: 8px;
+
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;;
+    font-size: 14px;
+    line-height: 1.75;
+    font-weight: 400;
+    color: white;
+    letter-spacing: 1.4px;
+    text-transform: uppercase;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  }
+
+  .spunk-btn:disabled {
+    color: rgba(0, 0, 0, 0.26);
+  }
+
+  .spunk-btn:hover {
+    cursor: pointer;
+  }
+
+  .spunk-btn-contained {
+    background-color: rgb(var(--primary));
+    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
+  }
+
+  .spunk-btn-contained:hover:not(:disabled) {
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0,0,0,.12);
+    filter: brightness(120%);
+  }
+
+  .spunk-btn-contained:disabled {
+    box-shadow: none;
+    background-color: rgba(0, 0, 0, 0.12);
+  }
+
+  .spunk-btn-outlined {
+    background-color: transparent;
+    border: 1px solid rgb(var(--primary));
+    color: rgb(var(--primary));
+  }
+
+  .spunk-btn-outlined:disabled {
+    border: 1px solid rgba(0, 0, 0, 0.12);
+  }
+
+  .spunk-btn-text {
+    background-color: transparent;
+    color: rgb(var(--primary));
+  }
+
+  .spunk-btn-text:hover:not(:disabled)  {
+    background-color: rgba(var(--primary), 0.05);
+  }
+</style>
+
