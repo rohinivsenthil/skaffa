057b3379725015534d5396d5dba9632a87458464
Rohini Senthil <rohinivsenthil@gmail.com>
2020-12-08 19:01:46 +0530

    (improv) add more button props

--- src/components/Button/index.svelte
+++ src/components/Button/index.svelte
@@ -1,26 +1,24 @@
 <script>
-  export let variant = 'contained';
+  export let variant = "contained";
   export let onClick;
   export let disabled = false;
+  export let fullWidth = false;
+  export let disableElevation = false;
+  export let style;
   /**
    * TODO: add in following props
    */
-  // export let disableElevation = 'false';
-  // export let fullWidth = 'false';
+  // class
+  // href
   // export let size = 'medium';
   // export let color = 'primary'
 
-  const btnClass = 'spunk-btn ' + `spunk-btn-${variant}`;
+  const btnClass =
+    "spunk-btn " +
+    `spunk-btn-${variant} ` +
+    `${fullWidth ? "spunk-btn-full-width " : ""}` +
+    `${disableElevation ? "spunk-btn-disable-elevation " : ""}`;
 </script>
-
-<button 
-  class={btnClass}
-  on:click={onClick}
-  disabled={disabled}
-  type="button"
->
-  <slot></slot>
-</button>
 
 <style>
   .spunk-btn {
@@ -36,18 +34,20 @@
     border-radius: 4px;
     margin: 8px;
 
-    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;;
+    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
     font-size: 14px;
     line-height: 1.75;
     font-weight: 400;
-    color: white;
     letter-spacing: 1.4px;
     text-transform: uppercase;
-    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    text-decoration: none;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   }
 
   .spunk-btn:disabled {
-    color: rgba(0, 0, 0, 0.26);
+    color: rgba(var(--black), 0.26);
   }
 
   .spunk-btn:hover {
@@ -56,17 +56,22 @@
 
   .spunk-btn-contained {
     background-color: rgb(var(--primary));
-    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
+    color: rgb(var(--white));
+    box-shadow: 0px 3px 1px -2px rgba(var(--black), 0.2),
+      0px 2px 2px 0px rgba(var(--black), 0.14),
+      0px 1px 5px 0px rgba(var(--black), 0.12);
   }
 
   .spunk-btn-contained:hover:not(:disabled) {
-    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0,0,0,.12);
+    box-shadow: 0px 2px 4px -1px rgba(var(--black), 0.2),
+      0px 4px 5px 0px rgba(var(--black), 0.14),
+      0px 1px 10px 0px rgba(var(--black), 0.12);
     filter: brightness(120%);
   }
 
   .spunk-btn-contained:disabled {
     box-shadow: none;
-    background-color: rgba(0, 0, 0, 0.12);
+    background-color: rgba(var(--black), 0.12);
   }
 
   .spunk-btn-outlined {
@@ -76,7 +81,7 @@
   }
 
   .spunk-btn-outlined:disabled {
-    border: 1px solid rgba(0, 0, 0, 0.12);
+    border: 1px solid rgba(var(--black), 0.12);
   }
 
   .spunk-btn-text {
@@ -84,8 +89,23 @@
     color: rgb(var(--primary));
   }
 
-  .spunk-btn-text:hover:not(:disabled)  {
+  .spunk-btn-text:hover:not(:disabled) {
     background-color: rgba(var(--primary), 0.05);
+  }
+
+  .spunk-btn-full-width {
+    width: 100%;
+  }
+
+  .spunk-btn-disable-elevation:not(:disabled) {
+    box-shadow: none;
+  }
+
+  .spunk-btn-disable-elevation:hover:not(:disabled) {
+    box-shadow: none;
   }
 </style>
 
+<button class={btnClass} on:click={onClick} {disabled} type="button" {style}>
+  <slot />
+</button>
